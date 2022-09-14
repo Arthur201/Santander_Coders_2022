5ed6b33f07770e5d08a2fa6f3fb6b10e012a6bde
Arthur201 <arthurlaureanosilva2001@gmail.com>
2022-09-14 16:32:05 -0300

    Gŕafico Covid-19 Brasil finalizado

--- Python Basics/Projeto_COVID19.py
+++ Python Basics/Projeto_COVID19.py
@@ -1,4 +1,9 @@
+from dataclasses import dataclass
 import requests as r
+import datetime as dt
+import csv
+
+
 
 url = 'https://api.covid19api.com/dayone/country/brazil'
 
@@ -23,11 +28,6 @@ DATA = 4
 for i in range(1,len(dados_finais)):
     dados_finais[i][DATA] = dados_finais[i][DATA][:10]
     
-
-
-import datetime as dt
-import csv
-
 with open('dados-covid-brasil.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerows(dados_finais)
@@ -35,8 +35,82 @@ with open('dados-covid-brasil.csv', 'w') as file:
 for i in range(1,len(dados_finais)):
     dados_finais[i][DATA] = dt.datetime.strptime(dados_finais[i][DATA],'%Y-%m-%d')
 
-print(dados_finais)
+#Criação do gráfico
 
+def get_datasets(y, labels):
+    if type(y[0]) == list:
+        datasets = []
+        for i in range(len(y)):
+            datasets.append({
+            
+                'label': labels[i],
+                'data': y[i]
+            })
+                   
 
+        return datasets
+    else:
+        return[{
+            'label': labels[0],
+            'data': y
+        }]
 
+def set_title(title=''):
+    if title != '':
+        display = 'true'
+    else:
+        display = 'false'
+    return {
+        'title': title,
+        'display': display
+    }
 
+def create_chart(x, y, labels, kind='bar', title=''):
+
+    datasets = get_datasets(y, labels)
+    options = set_title(title)
+
+    chart = {
+        'type': kind,
+        'data': {
+            'labels': x,
+            'datasets':datasets
+        },
+        'options':options
+    }
+
+    return chart
+
+def get_api_chart(chart):
+    url_base = 'https://quickchart.io/chart'
+    resp = r.get(f'{url_base}?c={str(chart)}')
+    return resp.content
+
+def save_image(path, content):
+    with open(path, 'wb') as image:
+        image.write(content)
+
+from PIL import Image
+from IPython.display import display
+
+def display_image(path):
+    img_pil =Image.open(path)
+    display(img_pil)
+
+y_data_1 = []
+for obs in dados_finais[1::10]:
+    y_data_1.append(obs[CONFIRMADOS])
+y_data_2 = []
+for obs in dados_finais[1::10]:
+    y_data_2.append(obs[RECUPERADOS])
+
+labels = ['Confirmados','Recuperados']
+
+x = []
+for obs in dados_finais[1::10]:    
+    x.append((obs[DATA]).strftime('%d/%m/%Y'))
+
+chart = create_chart(x, [y_data_1,y_data_2], labels, title='Gráfico de Confirmados VS Recuperados')
+chart_content = get_api_chart(chart)
+save_image('meu-primeiro-gráfico.png',chart_content)
+display_image('meu-primeiro-gráfico.png')
